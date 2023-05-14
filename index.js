59fef89b8354257f355f717f02418bbebf920dbe
tsopsericado <ricardotsopse@gmail.com>
2023-05-14 09:13:49 +0100

    variables declare in js file

---
+++ index.js
@@ -0,0 +1,6 @@
+let encodeBtn = document.querySelector('#btn');
+let message = document.querySelector('#msg');
+let normal = document.querySelector('#normalText');
+let chunk = document.querySelector('#normalText');
+let encode = document.querySelector('#encodeText');
+let err = document.querySelector('#pText')
